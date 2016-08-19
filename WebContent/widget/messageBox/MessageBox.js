54795da6069ca1750a964d6f65edbadacec1417d
Hu, Xiaoyi <evan.xy.hu@philips.com>
2016-08-19 16:11:26 +0800

    add

--- WebContent/widget/messageBox/MessageBox.js
+++ WebContent/widget/messageBox/MessageBox.js
@@ -23,16 +23,141 @@ define([
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
     'dojo/_base/declare',
+    'dojo/dom-style',
     'dojo/text!./message-box.html',
     'xstyle/css!./message-box.css'
-], function(_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, declare, template) {
+], function(_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, declare, style, template) {
     return declare('', [
         _WidgetBase,
         _TemplatedMixin,
         _WidgetsInTemplateMixin
     ], {
 
-        templateString : template
+        /**
+         * template string
+         */
+        templateString : template,
 
+        /**
+         * including: error, attention, confirm, information, message
+         */
+        state : null,
+
+        /**
+         * lime, blue, gray, closeIcon
+         */
+        type : null,
+
+        /**
+         * the content displayed on box. Default is ''
+         */
+        content : '',
+
+        /**
+         * state config
+         */
+        _stateConfig : {
+            error : {
+                title : 'Error',
+                button : {
+                    cancel : 'Acknowledge'
+                }
+            },
+            attention : {
+                title : 'Attention',
+                button : {
+                    cancel : 'Close'
+                }
+            },
+            confirm : {
+                title : 'Confirm',
+                button : {
+                    ok : 'OK',
+                    cancel : 'Cancel'
+                }
+            },
+            information : {
+                title : 'Information',
+                button : {
+                    ok : 'OK'
+                }
+            },
+            message : {
+                button : {
+                    ok : 'Done'
+                }
+            }
+        },
+
+        /**
+         * dialog title
+         */
+        _title : '',
+
+        /**
+         * ok button title
+         */
+        _okTitle : '',
+
+        /**
+         * cancel button title
+         */
+        _cancelTitle : '',
+
+        /**
+         * options:
+         * 
+         * 1. state:
+         * 
+         * 2. content:
+         * 
+         */
+        constructor : function(options) {
+            // handle options
+            var state = options.state;
+            var type = options.type;
+            var title = options.title;
+
+            var sc = this._stateConfig[state];
+            if (state && sc) {
+                // class name
+                this.baseClass = state + ' ' + (type ? type : '');
+
+                // title & button
+                this._title = sc.title || title || '';
+                this._okTitle = sc.button.ok || '';
+                this._cancelTitle = sc.button.cancel || '';
+
+            }
+        },
+
+        /**
+         * on close
+         */
+        _onClose : function() {
+            this.destroy();
+        },
+
+        /**
+         * on cancel
+         */
+        _onCancel : function() {
+            if (this.onCancel) {
+                this.onCancel();
+            }
+
+            this._onClose();
+        },
+
+        /**
+         * on ok
+         */
+        _onOk : function() {
+            if (this.onOk) {
+                this.onOk();
+            }
+
+            this._onClose();
+        }
     });
 });
